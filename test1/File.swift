f9557835e47c94f0432c786af0eb760187602bf4
tarek ghamraoui <2037077@collegeahuntsic.qc.ca>
2024-08-30 11:46:49 -0400

    Initial Commit

---
+++ test1/File.swift
@@ -0,0 +1,33 @@
+//
+//  File.swift
+//  test1
+//
+//  Created by Tarek Ghamraoui on 2024-08-19.
+//
+
+import Foundation
+
+//exemple 1
+let a = 0
+let b = 1
+let c = 2
+
+//exemple 2
+let maConstante = 42
+var maVariable = "bonjour"
+
+//exemple 3
+//func maFonction(parametre1 : Int, parametre2 : String) -> String {
+
+
+
+
+// Affiche "Bonjour le monde"
+
+//exemple 5
+
+
+
+
+
+
---
+++ test1/test1App.swift
@@ -0,0 +1,17 @@
+//
+//  test1App.swift
+//  test1
+//
+//  Created by Tarek Ghamraoui on 2024-08-19.
+//
+
+import SwiftUI
+
+@main
+struct test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
